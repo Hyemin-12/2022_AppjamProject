730cf232cb244a999cfd0a54d650cb9dc8e0dae8
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-08-14 07:02:04 +0900

    버그 수정

---
+++ AppjamProject/app/src/main/java/com/example/appjamproject/MyPageFragment.kt
@@ -0,0 +1,25 @@
+package com.example.app_jamproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.appjamproject.R
+
+class MyPageFragment : Fragment() {
+    private lateinit var image_view: ImageView
+    val OPEN_CALLERY = 1
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.my_page_fragment,container,false)
+        return view
+    }
+}
